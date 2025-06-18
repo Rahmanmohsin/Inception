74703b101df4d6f9ef2b94313a312858ea81559a
mohrahma <mohsinrahman901@gmail.com>
2025-06-19 01:20:53 +0200

    Log 13]: all works [TODO] clean up files

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -12,26 +12,21 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 	-subj "/C=DE/ST=Baden-Wuerttemberg/L=Heilbronn/OU=42 Heilbronn Students/CN=$DOMAIN"
 
 cat > "$SITES_DIR/default" <<EOF
-
 server {
-    listen 443 ssl http2;
-    server_name $DOMAIN;
-
-    ssl_certificate $SSL_DIR/fullchain.pem;
-    ssl_certificate_key $SSL_DIR/key.pem;
-    
-    ssl_session_timeout 1d;
-    ssl_session_cache shared:SSL:10m;
-    ssl_session_tickets off;
-    
-    ssl_protocols TLSv1.3;
-    ssl_prefer_server_ciphers off;
-    
-    location / {
+        listen 443 ssl;
         root /var/www/html;
-        index index.html;
+        server_name $DOMAIN www.$DOMAIN;
+        index index.php;
+
+        ssl_certificate $SSL_DIR/fullchain.pem;
+        ssl_certificate_key $SSL_DIR/key.pem;
+        ssl_protocols TLSv1.3;
+
+        location ~ \.php$ {
+            include snippets/fastcgi-php.conf;
+            fastcgi_pass wordpress:9000;
+        }
     }
-}
 EOF
 
 ln -sf "$SITES_DIR/default" "/etc/nginx/sites-enabled/default"
